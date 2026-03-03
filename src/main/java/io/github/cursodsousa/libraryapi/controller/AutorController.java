4afae254c150fd3cc4d090b49766f2b0efecbd27
Marcos Moraes <msouzamarcosm@gmail.com>
2026-03-03 15:07:42 -0300

    ultima atualização

--- src/main/java/io/github/cursodsousa/libraryapi/controller/AutorController.java
+++ src/main/java/io/github/cursodsousa/libraryapi/controller/AutorController.java
@@ -106,7 +106,7 @@ public class AutorController {
     @PutMapping("{id}")
     public ResponseEntity<Object> atualizar(
             @PathVariable("id") String  id,
-            @RequestBody AutorDTO dto){
+            @RequestBody @Valid AutorDTO dto){
         try {
             var idAutor = UUID.fromString(id);
             Optional<Autor> autorOptional = service.obterPorid(idAutor);
--- src/main/java/io/github/cursodsousa/libraryapi/controller/dto/AutorDTO.java
+++ src/main/java/io/github/cursodsousa/libraryapi/controller/dto/AutorDTO.java
@@ -4,6 +4,8 @@ package io.github.cursodsousa.libraryapi.controller.dto;
 import io.github.cursodsousa.libraryapi.model.Autor;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Past;
+import jakarta.validation.constraints.Size;
 
 import java.time.LocalDate;
 import java.util.UUID;
@@ -11,10 +13,13 @@ import java.util.UUID;
 public record AutorDTO(
         UUID id,
         @NotBlank(message = "Campo obrigatório")
+        @Size(min =2, max = 100, message = "campo fora do tamanho padrão")
         String nome,
         @NotNull(message = "Campo obrigatório")
+        @Past (message = "não pode ser uma data futura")
         LocalDate dataNascimento,
         @NotBlank(message = "Campo obrigatório")
+        @Size (min = 2, max = 50, message = "campo fora do tamanho padrão")
         String nacionalidade) {
 
 
